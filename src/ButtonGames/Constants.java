15ed7198e49c54c2b1fd026bcd4378f9b7d49b68
BT_1N3_34@BKTECH.LOCAL <BT_1N3_34@BKTECH.LOCAL>
2019-01-03 11:42:43 -0500

    20190103

--- src/ButtonGames/Constants.java
+++ src/ButtonGames/Constants.java
@@ -1,5 +1,7 @@
 package ButtonGames;
 
 public class Constants {
+    public static final String USERNAME = "null";
     public static final int NUMBER_OF_BUTTONS = 4;
+    public static final int BASE_BUTTON_COUNT = 1;
 }
--- src/ButtonGames/GameCode.java
+++ src/ButtonGames/GameCode.java
@@ -1,39 +1,75 @@
 package ButtonGames;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameCode extends Application {
-    @FXML
-    public void buttonPressed(ActionEvent actionEvent) {
+    private int score = 0;
+    private int button_count = Constants.BASE_BUTTON_COUNT;
+
+    private Button[] buttons;
+
+    private List<Integer> inputPattern = new ArrayList();
+    private List<Integer> realPattern = new ArrayList();
+
+    public void generatePattern() {
+        this.realPattern.clear();
+
+        for (int i = 0; i < button_count; i++)
+            this.realPattern.add((int) (Math.random() * Constants.BASE_BUTTON_COUNT));
+        this.button_count++;
     }
 
     @Override
     public void start(Stage primaryStage) {
-        GridPane root = new GridPane();
-        Scene scene = new Scene(root, 300, 275);
-
+        VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
-        root.setHgap(10);
-        root.setVgap(10);
+        root.setSpacing(10);
+
+        HBox buttonContainer = new HBox();
+        buttonContainer.setAlignment(Pos.CENTER);
+        buttonContainer.setSpacing(10);
 
         for (int i = 0; i < Constants.NUMBER_OF_BUTTONS; i++) {
             Button button = new Button(String.valueOf(i));
 
+            button.setDisable(true);
             button.getStyleClass().add("fake_lights");
-            root.add(button, i, 0);
+            buttonContainer.getChildren().add(button);
         }
+
+        HBox controlContainer = new HBox();
+        controlContainer.setAlignment(Pos.CENTER);
+        controlContainer.setSpacing(10);
+
+        TextField userTextField = new TextField();
+        userTextField.setPromptText("Enter your username: " + Constants.USERNAME);
 
         Button initGame = new Button("Start Game");
         initGame.getStyleClass().add("init");
-        root.addRow(1, initGame);
+        initGame.setOnAction(e -> {
+            initGame.setDisable(true);
+            userTextField.setEditable(false);
+            buttonContainer.getChildren().forEach(button -> button.setDisable(false));
 
+            if (userTextField.getText().equals("")) {
+                userTextField.setText("null");
+            }
+        });
+
+        controlContainer.getChildren().addAll(initGame, userTextField);
+        root.getChildren().addAll(buttonContainer, controlContainer);
+
+        Scene scene = new Scene(root, 300, 275);
         scene.getStylesheets().add(this.getClass().getResource("display.css").toExternalForm());
         primaryStage.setTitle("Simon Says");
         primaryStage.setScene(scene);
