8e8b572cac6aff90c4e8bc4ed111cfe34633ad83
Edmund Wu <fangkazuto@gmail.com>
2019-01-06 09:05:12 -0500

    Declare colors in Constants

--- src/ButtonGames/Constants.java
+++ src/ButtonGames/Constants.java
@@ -1,6 +1,11 @@
 package ButtonGames;
 
 public class Constants {
+    // Colors
+    public static final String LIGHT_BG = "#ff8a65";
+    public static final String DARK_BG = "#ff3d00";
+
+    // Game
     public static final String USERNAME = "null";
     public static final int NUMBER_OF_BUTTONS = 4;
     public static final int BASE_BUTTON_COUNT = 1;
